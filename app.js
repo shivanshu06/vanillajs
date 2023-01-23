54d7b5bb273faf853ad41a39ddc5191c99c2a782
shivanshu06 <102288010+shivanshu06@users.noreply.github.com>
2023-01-23 20:16:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-var btnTranslate=document.querySelector("#btn-translate");
-var txtinput=document.querySelector("#txt-input");
-var textoutput=document.querySelector("#output")
+var btnTranslate = document.querySelector("#btn-translate");
+var txtinput = document.querySelector("#txt-input");
+var textoutput = document.querySelector("#output")
 
 
-var serverURL=" https://api.funtranslations.com/translate/minion.json"
+var serverURL = " https://api.funtranslations.com/translate/minion.json"
 
-function getTranslationURL(input){
-    return serverURL + "?" + "text="+input
+function getTranslationURL(input) {
+    return serverURL + "?" + "text=" + input
 
 }
 
-function errorHandler(error){
-    console.log("error occured",error);
-    alert("server error try again later")
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("sorry server error!!!! try again later")
 }
 
-function clickHandler(){
-    var inputtext=txtinput.value;
+function clickHandler() {
+    var inputtext = txtinput.value;
 
     fetch(getTranslationURL(inputtext))
-    .then(response => response.json())
-    .then(json => {
-        var translatedtxt=json.contents.translated;
-        textoutput.innerText=translatedtxt
-    })
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedtxt = json.contents.translated;
+            textoutput.innerText = translatedtxt
+        })
+        .catch(errorHandler)
 }
-btnTranslate.addEventListener("click",clickHandler)
+btnTranslate.addEventListener("click", clickHandler)
